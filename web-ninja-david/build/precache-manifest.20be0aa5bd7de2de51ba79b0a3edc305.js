d983f4e78926788caf1deaf949d0f001a2ae72b0
David Scherer-ODell <dscherer21@gmail.com>
2020-09-19 16:00:40 -0500

    creating and adding build file

---
+++ web-ninja-david/build/precache-manifest.20be0aa5bd7de2de51ba79b0a3edc305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b30cc5b307d9af221c3b526e998752f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22868a27ad140ea0889a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc235a6e20b501b4bc82",
+    "url": "/static/js/2.4c893ac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c893ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22868a27ad140ea0889a",
+    "url": "/static/js/main.796352f6.chunk.js"
+  },
+  {
+    "revision": "233f2e351a808d352555",
+    "url": "/static/js/runtime-main.e8ae2e29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
