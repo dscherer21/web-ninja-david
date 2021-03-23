6630506eb008d8b2d19c924c2ba90a4038d33aa7
David Scherer-ODell <dscherer21@gmail.com>
2021-03-23 14:21:02 -0500

    added in social media links to the social media links section

--- web-ninja-david/src/components/socialMediaLinks.js
+++ web-ninja-david/src/components/socialMediaLinks.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 function SocialMediaLinks() {
   
   return (
     <section>
         This is the Social Media Links Section!
+        <ul>
+          <li>
+            <a href='https://www.linkedin.com/in/david-scherer-odell/' target='_blank' rel="noopener noreferrer">Linkedin</a>
+          </li>
+          <li>
+            <a href='https://www.facebook.com/david.scherer.77' target='_blank' rel="noopener noreferrer">Facebook</a>
+          </li>
+          <li>
+            <a href='https://github.com/dscherer21' target='_blank' rel="noopener noreferrer">Github</a>
+          </li>
+        </ul>
     </section>
   );
 }
