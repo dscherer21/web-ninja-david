1f21f51c713b23ae1b9a2eda4da7a7d93352652a
David Scherer-ODell <dscherer21@gmail.com>
2021-03-24 11:32:28 -0500

    added projectsTemplate for mapping, added images to the media folder

--- web-ninja-david/src/components/projectsPage.js
+++ web-ninja-david/src/components/projectsPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import projects from './projects';
+import ProjectsTemplate from './projectsTemplate';
 
 function ProjectsPage() {
   
   return (
     <main>
         <div>This is the ProjectsPage body!</div>
+        <ProjectsTemplate/>
     </main>
   );
 }
---
+++ web-ninja-david/src/components/projectsTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProjectsTemplate() {
+  
+  return (
+    <div>
+        This is the Projects Template!
+    </div>
+  );
+}
+
+export default ProjectsTemplate;
