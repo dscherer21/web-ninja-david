bb4efedd8311358a3068696d62b2cfa5127e87a7
David Scherer-ODell <dscherer21@gmail.com>
2021-04-08 00:00:20 -0500

    removing env.js from repo

--- web-ninja-david/build/env.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  "CAPTCHA_SITE_KEY": "6Leal50aAAAAAPAF_AgxDPLFdAdBCtASrZIWkBwA",
-  "CAPTCHA_SECRET_KEY": "6Leal50aAAAAALClkwwwSGOlH4Sdr-8ej3rUrHiL",
-  "SENDING_EMAIL": "dscherer21formsender@outlook.com",
-  "SENDING_EMAIL_PASSWORD": "y3Yt5RiLUmxNYEZ"
-};
