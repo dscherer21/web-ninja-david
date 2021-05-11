6bd50942e58dd4a87e64a841a81909381e7a28bc
David Scherer-ODell <dscherer21@gmail.com>
2021-05-11 14:38:14 -0500

    trying to setup the test environment

--- web-ninja-david/src/App.test.js
+++ web-ninja-david/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+import { act, renderHook } from 'react-dom/test-utils';
+import { shallow, configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+
+//Configuring Enzyme
+configure({ adapter: new Adapter() });
+
+test('Sample Test', () =>{
+  expect(true).toBeTruthy();
+});
+
+test('renders Welcome in the document', () => {
+  const { getByText } = render(App);
+  const linkElement = getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 });
