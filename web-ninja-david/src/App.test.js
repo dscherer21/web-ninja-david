95ff8505970681ab28f02bb44dd62ca58364aaac
David Scherer-ODell <dscherer21@gmail.com>
2021-05-14 14:32:19 -0500

    imported all components into test suite and created a basic renders without crashing test for each one

--- web-ninja-david/src/App.test.js
+++ web-ninja-david/src/App.test.js
@@ -6,7 +6,15 @@ import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from './App.js';
 import AboutDavid from './components/aboutDavid';
-
+import Header from './components/header';
+import LandingPage from './components/landingPage';
+import Footer from './components/footer';
+import ProjectsPage from './components/projectsPage';
+import ContactPage from './components/contactPage';
+import SocialMediaLinks from './components/socialMediaLinks';
+import AudioPlayer from './components/audioPlayer';
+import EmailForm from './components/emailForm';
+import projects from './components/projects';
 
 //Configuring Enzyme
 configure({ adapter: new Adapter() });
@@ -24,11 +32,92 @@ describe('App functionality', () => {
 
 });
 
-describe('aboutDavid functionality', () => {
+describe('AboutDavid functionality', () => {
 
   test('renders without crashing', () => {
     let wrapper = shallow(<AboutDavid />);
     expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('Header functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<Header />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('LandingPage functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<LandingPage />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('Footer functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<Footer />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('ProjectsPage functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<ProjectsPage />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('ContactPage functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<ContactPage />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('SocialMediaLinks functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<SocialMediaLinks />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('AudioPlayer functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<AudioPlayer />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('EmailForm functionality', () => {
+
+  test('renders without crashing', () => {
+    let wrapper = shallow(<EmailForm />);
+    expect(wrapper).not.toBeNull();
+  });
+
+});
+
+describe('projects array', () => {
+
+  test('project index 0 contains title and id', () =>{
+    expect(projects[0].title).toContain("Websmith David");
+    expect(projects[0]._id).toBe(1);
   });
 
 });
